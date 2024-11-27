f20d9b228f8e1603dc73440872bbc245800e7f90
Tien <hoangminhtien.dev@gmail.com>
2024-11-27 12:31:31 +0700

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,50 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyC104_NrGRpbecYslnOIgR8Y9EG_lWgBTQ",
+    authDomain: "fir-chat-e06df.firebaseapp.com",
+    projectId: "fir-chat-e06df",
+    storageBucket: "fir-chat-e06df.firebasestorage.app",
+    messagingSenderId: "912644726160",
+    appId: "1:912644726160:web:ba4cfdbb81667b90452715",
+    measurementId: "G-F25D8GFK29"
   },
 };
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC104_NrGRpbecYslnOIgR8Y9EG_lWgBTQ",
+//   authDomain: "fir-chat-e06df.firebaseapp.com",
+//   projectId: "fir-chat-e06df",
+//   storageBucket: "fir-chat-e06df.firebasestorage.app",
+//   messagingSenderId: "912644726160",
+//   appId: "1:912644726160:web:ba4cfdbb81667b90452715",
+//   measurementId: "G-F25D8GFK29"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+
+// export const environment = {
+//   firebase: {
+//     apiKey: 'API_KEY',
+//     authDomain: 'PROJECT_ID.firebaseapp.com',
+//     databaseURL: 'https://PROJECT_ID.firebaseio.com',
+//     projectId: 'PROJECT_ID',
+//     storageBucket: 'PROJECT_ID.appspot.com',
+//     messagingSenderId: 'SENDER_ID',
+//     appId: 'APP_ID',
+//     measurementId: 'G-MEASUREMENT_ID',
+//   },
+// };
